555d897fcc71dee3270e8072a579c981bc12a67c
Ramona Zmole <ramona.zmole@gmail.com>
2021-04-28 17:04:08 +0300

    check api package

---
+++ UIFrameworkAndNUnit/UIFrameworkAndNUnit/CreateRoomInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIFrameworkAndNUnit
+{
+    public class CreateRoomInput
+    {
+        public string roomNumber { get; set; } = Faker.RandomNumber.Next(1, 200).ToString();
+        public string type { get; set; } = "Twin";
+        public string accessible { get; set; } = Faker.Boolean.Random().ToString();
+        public string description { get; set; } = Faker.Lorem.Sentence();
+        public string image { get; set; } = "https://www.mwtestconsultancy.co.uk/img/room1.jpg";
+        public string roomPrice { get; set; } = Faker.RandomNumber.Next(1, 200).ToString();
+        public List<string> features { get; set; } = new List<string>();
+
+    }
+}
--- UIFrameworkAndNUnit/UIFrameworkAndNUnit/UnitTest1.cs
+++ UIFrameworkAndNUnit/UIFrameworkAndNUnit/UnitTest1.cs
@@ -1,17 +1,40 @@
 using FluentAssertions;
+using Newtonsoft.Json;
+using NsTestFrameworkApi.RestSharp;
 using NsTestFrameworkUI.Helpers;
 using NUnit.Framework;
+using RestSharp;
 
 namespace UIFrameworkAndNUnit
 {
     public class Tests : BaseTest
     {
+        private readonly RestClient _client = RequestHelper.GetRestClient("https://automationintesting.online/");
+        private int _roomId;
+
+        [SetUp]
+        public override void Before()
+        {
+            _client.AddDefaultHeader("cookie", "__cfduid=da238cb8c831b6c8851619a317d638e571616569469; _ga=GA1.2.289307235.1616569470; _gid=GA1.2.1338565523.1616569470; banner=true; token=4UzjLD4mJGFgy1E1; _gat=1");
+            var response = _client.CreateRequest("/room/", new CreateRoomInput(), Method.POST);
+            _roomId = JsonConvert.DeserializeObject<CreateRoomOutput>(response.Content).roomId;
+            base.Before();
+        }
+
         [Test]
         public void Test1()
         {
+
             Browser.GoTo("https://nunit.org/");
-             Browser.WebDriver.Url.Should().Be("https://nunit.org/234");
-          //  Assert.That(Browser.WebDriver.Url, Is.EqualTo("https://nunit.org/234"));
+            Browser.WebDriver.Url.Should().Be("https://nunit.org/234");
+            //  Assert.That(Browser.WebDriver.Url, Is.EqualTo("https://nunit.org/234"));
+        }
+
+        [TearDown]
+        public override void After()
+        {
+            base.After();
+            _client.CreateRequest($"/room//{_roomId}", Method.DELETE);
         }
     }
 }
